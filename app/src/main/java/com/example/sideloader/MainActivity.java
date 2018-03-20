1858c2a3c1ed5248293a2af706ed1945773249f1
Pittman <wpittman@paypal.com>
2018-03-20 13:02:12 -0500

    created repo. First commit

---
+++ app/src/main/java/com/example/sideloader/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sideloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//copied from git
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
